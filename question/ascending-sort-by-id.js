279c417b1992b6e671e3fc8bee7986c131619651
RyotaHirano <hirano@tsumikiinc.com>
2015-10-08 12:47:08 +0900

    [add]  ascending-sort-by-id passing

--- question/ascending-sort-by-id.js
+++ question/ascending-sort-by-id.js
@@ -14,5 +14,20 @@
 */
 
 export default function(array) {
+  var sortArray = [];
 
+  array.forEach(function(data, i){
+    sortArray[i] = data;
+  });
+
+  sortArray.sort(function(a, b) {
+    if (a.id < b.id) {
+      return -1;
+    } else {
+      return 1;
+    }
+    return 0;
+  });
+
+  return sortArray;
 }
