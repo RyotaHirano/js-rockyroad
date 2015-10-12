51d7e761da78ae356d8c09d74811fae25e776e71
Ryota Hirano <hirano@tsumikiinc.com>
2015-10-12 16:49:46 +0900

    [add] get-max-number passing

--- question/get-max-number.js
+++ question/get-max-number.js
@@ -7,5 +7,98 @@
 */
 
 export default function(array) {
+  var tmpArray = [];
+  var searchIndex = 0;
+  var nowIsRarge = false;
+  var maxNumber = '';
 
+  function addArray(value) {
+    var pushArray = false;
+    var nowRarge = false;
+    var nowIndex = 0;
+
+    if (tmpArray.length > 0) {
+      var _tmpArray = tmpArray.slice();
+
+      _tmpArray.some(function(data, index) {
+        nowIndex = index;
+        nowRarge = whitchRargeValue(value, data);
+        if (nowRarge) {
+          pushArray = true;
+          tmpArray.splice(nowIndex, 0, value);
+          return true;
+        }
+      });
+    } else {
+      if (!pushArray) {
+        return tmpArray.push(value);
+      }
+    }
+    if (!pushArray) {
+      return tmpArray.push(value);
+    }
+
+    return false;
+  }
+
+  function whitchRargeValue(p, c) {
+    var nextIndex = searchIndex + 1;
+    var pFirstValue = parseInt(p.slice(searchIndex, nextIndex));
+    var cFirstValue = parseInt(c.slice(searchIndex, nextIndex));
+
+    if (!isNaN(pFirstValue) && !isNaN(cFirstValue)) {
+      if (pFirstValue != cFirstValue) {
+        if (pFirstValue > cFirstValue) {
+          nowIsRarge = true;
+        } else {
+          nowIsRarge = false;
+        }
+      } else {
+        searchIndex++;
+        whitchRargeValue(p, c);
+      }
+    } else {
+      if (!isNaN(pFirstValue)) {
+        var pBeforeFirstValue = parseInt(p.slice(searchIndex - 1, nextIndex - 1));
+        if (!isNaN(pBeforeFirstValue)) {
+          if (pBeforeFirstValue > pFirstValue) {
+            nowIsRarge = false;
+          } else {
+            nowIsRarge = true;
+          }
+        } else {
+          nowIsRarge = true;
+        }
+      } else {
+        var cBeforeFirstValue = parseInt(c.slice(searchIndex - 1, nextIndex - 1));
+        if (isNaN(cBeforeFirstValue)) {
+          if (cBeforeFirstValue > cFirstValue) {
+            nowIsRarge = false;
+          } else {
+            nowIsRarge = true;
+          }
+        } else {
+          nowIsRarge = true;
+        }
+      }
+    }
+
+    searchIndex = 0;
+    return nowIsRarge;
+  }
+
+  function getMaxNumber(array) {
+    var _array = array.slice().map(function(value) {
+      var strValue = value.toString(10);
+      addArray(strValue);
+    }, 0);
+
+    tmpArray.map(function(data) {
+      maxNumber = maxNumber + data + '';
+    });
+
+    return parseInt(maxNumber);
+  }
+
+  return getMaxNumber(array);
 }
