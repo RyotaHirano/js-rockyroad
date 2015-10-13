12e88f8c5771698535cec2da1fb6a7cd06bd5e3a
RyotaHirano <hirano@tsumikiinc.com>
2015-10-13 22:06:35 +0900

    [add] concat-keys passing

--- question/concat-keys.js
+++ question/concat-keys.js
@@ -20,5 +20,55 @@
 */
 
 export default function(array) {
+  var tmpObject = {};
 
+function isObject(value) {
+	return (Object(value) === value && !Array.isArray(value));
+}
+
+function getData(obj) {
+	var returnArray = [];
+	for(var _key in obj) {
+		if(obj.hasOwnProperty(_key)) {
+			returnArray.push(obj[_key]);
+		}
+	}
+
+	return returnArray;
+}
+
+function pushArray(array, value) {
+  if (array.indexOf(value) < 0) {
+    array.push(value);
+  }
+}
+
+function concatKeys(array) {
+	array.slice().map(function(data){
+		for (var key in data) {
+			var tmpArray = [];
+			var tmpGetArray = [];
+
+			if (data.hasOwnProperty(key)) {
+				if(Array.isArray(tmpObject[key])) {
+					tmpArray = tmpObject[key];
+				}
+				if (isObject(data[key])){
+					tmpGetArray = getData(data[key]);
+					tmpGetArray.map(function(_data){
+						pushArray(tmpArray, _data);
+					});
+				} else {
+					pushArray(tmpArray, data[key]);
+				}
+
+				tmpObject[key] = tmpArray.sort();
+			}
+		}
+	});
+
+	return [tmpObject];
+}
+
+  return concatKeys(array);
 }
