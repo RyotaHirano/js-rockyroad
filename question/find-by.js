e36334c335d65a7c27e818ddc9f0c04600b96db1
RyotaHirano <hirano@tsumikiinc.com>
2015-10-13 13:50:36 +0900

    [add] find-by passing

--- question/find-by.js
+++ question/find-by.js
@@ -11,4 +11,40 @@
 
 export default function(array, object) {
 
+
+  function findBy(array, object) {
+    var _array = array.slice();
+    var result = {};
+    var keys = Object.keys(object);
+    var keysCount = keys.length;
+    var isHit = 0;
+
+    keys.map(function(key) {
+      var target = object[key];
+      _array.map(function(data, index) {
+        if (target.toString() === data[key].toString()) {
+          isHit = 1;
+        } else {
+          isHit = 0;
+        }
+
+        if (isNaN(parseInt(result[index]))) {
+          result[index] = 0;
+        }
+        result[index] = parseInt(result[index]) + isHit;
+      });
+    });
+
+    for (var r_key in result) {
+      if (result.hasOwnProperty(r_key)) {
+        if (result[r_key] == keysCount) {
+          return _array[r_key];
+        }
+      }
+    }
+
+    return null;
+  }
+
+  return findBy(array, object);
 }
